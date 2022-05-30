555ac7f49c2c72d634693c182c02b789a91f1d51
Yuhua Zong <yhzong@umich.edu>
2022-05-30 12:11:14 -0700

    Add function to print student's t-test result in rmd

--- R/print_tests.R
+++ R/print_tests.R
@@ -1,15 +1,42 @@
 #' @title Chi-squared Test Result
-#' @description Print the result of a chi-squared test in a R markdown.
+#' @description Tidy the printout of a chi-squared test in a R markdown.
 #'
-#' @param test list of result from a chi-squared test
+#' @param test list of the result from a chi-squared test
 #'
 #' @return none
 #' @export
 
 print_chisq <- function(test) {
-  cat(" \n", paste("**", test$method, " of ",
-                   stringr::str_replace_all(test$data.name, "[a-z]*\\$", ""), "**", sep = ""),
+  cat(" \n",
+      paste0("**", test$method, " of ",
+            stringr::str_replace_all(test$data.name, "[a-z]*\\$", ""), "**"),
       " \n", names(test$statistic), "=", test$statistic,
-      " \n", "p-value =", test$p.value,
+      " \n", names(test$parameter), "=", test$parameter,
       " \n")
+  if (test$p.value < 0.001) {
+    cat(" p-value < 0.001 \n")
+  } else {
+    cat("p-value = ", test$p.value, " \n")
+  }
+}
+
+
+#' @title Student's T-test Result
+#' @description Tidy the printout of a student's t-test in a R markdown.
+#'
+#' @param test list of the result from a student's t-test
+#'
+#' @return none
+#' @export
+
+print_ttest <- function(test) {
+  cat(" \n", paste0("**", test$method, " of ", test$data.name, "**"),
+      " \n", names(test$statistic), "=", test$statistic, " \n")
+  if (test$p.value < 0.001) {
+    cat(" p-value < 0.001 \n")
+  } else {
+    cat(" p-value = ", round(test$p.value, 3), " \n")
+  }
+  cat(" ", names(test$estimate[1]), ":", round(test$estimate[1], 2), " \n",
+      names(test$estimate[2]), ":", round(test$estimate[2], 2), " \n")
 }
