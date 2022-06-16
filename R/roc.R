ceffd7a57897aad1b241cc8dc6522da452b104f6
Yuhua Zong <yhzong@umich.edu>
2022-06-15 23:18:05 -0700

    Add function to generate ROC curves

--- R/roc.R
+++ R/roc.R
@@ -1,5 +1,28 @@
-# Generate the ROC objects
-roc1 <- pROC::roc(df$outcome, df$predic)
-roc2 <- pROC::roc(df2$outcome, df2$predic)
+#' @title ROC for Model Performance
+#' @description Plot ROC curves of logistic regression models to assess model performance.
+#'
+#' @param ... ROC objects
+#' @param plot_title title for the plot as a string
+#'
+#' @return none
+#' @export
 
-pROC::ggroc(list(roc1, roc2))
+plot_roc <- function(..., plot_title) {
+  rocs <- list(roc1, roc2, roc3)
+  # Name each ROC object with AUC and 95% CIs
+  for (i in seq_along(rocs)) {
+    names(rocs)[i] <- paste0("AUC of model ", i, ": ",
+                             round(rocs[[i]][["ci"]][2], 2),
+                             " (", round(rocs[[i]][["ci"]][1], 2),
+                             ", ", round(rocs[[i]][["ci"]][3], 2), ")")
+  }
+
+  # Use linetype to differentiate curve aes = "linetype"
+  pROC::ggroc(rocs, legacy.axes = TRUE, size = 0.8) +
+    ggplot2::labs(title = plot_title) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.title = ggplot2::element_blank(),
+                   legend.position = c(1, 0.05),
+                   legend.justification = c(1, 0.05))
+
+}
