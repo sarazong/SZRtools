263390b79d96383aad9e85ee049e4618e52b0126
Yuhua Zong <yhzong@umich.edu>
2022-07-01 08:22:30 -0700

    Add horizontal barplot

--- R/see_cat_vars.R
+++ R/see_cat_vars.R
@@ -49,6 +49,54 @@ make_barplot1 <- function(data, cat_var, label_fmt = TRUE) {
 }
 
 
+#' @title Horizontal Barplot for Categorical Var
+#' @description Create a horizontal barplot for a categorical variable with percent
+#' and count labels. Good for variable that has greater than ten categories.
+#'
+#' @param data dataset that contains the categorical variable
+#' @param cat_var name of the categorical variable as a string
+#' @param label_fmt boolean to indicate whether plot labels should be formatted
+#'
+#' @return none
+#' @examples
+#' make_hbarplot(ggplot2::diamonds, "cut")
+#' @export
+
+make_hbarplot <- function(data, cat_var, label_fmt = TRUE) {
+
+  if (label_fmt) {
+    xlab <- stringr::str_to_title(stringr::str_replace(cat_var, "[:punct:]", " "))
+    plot_title <- paste0("Distribution of ", xlab)
+  } else {
+    xlab <- cat_var
+    plot_title <- paste0("Distribution of ", stringr::str_to_title(cat_var))
+  }
+
+  levels <- as.character(data %>%
+    dplyr::count(!!as.symbol(cat_var)) %>%
+    dplyr::arrange(n) %>%
+    dplyr::pull(!!as.symbol(cat_var)))
+  data <- data %>%
+    dplyr::mutate(!!as.symbol(cat_var) := factor(!!as.symbol(cat_var), levels = levels))
+
+  ggplot2::ggplot(data, ggplot2::aes(get(cat_var), fill = get(cat_var))) +
+    ggplot2::geom_bar(ggplot2::aes(y = ..count..)) +
+    ggplot2::geom_text(
+      ggplot2::aes(
+        label = paste0(..count.., " (", scales::percent(..prop.., accuracy = 0.1), ")"),
+        group = 1
+      ),
+      stat = "count",
+      hjust = -0.05
+    ) +
+    ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0.2))) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::labs(x = xlab, title = plot_title) +
+    ggplot2::coord_flip()
+}
+
+
 #' @title Barplot for Categorical by Categorical
 #' @description Create a barplot for a categorical variable grouped by another
 #' categorical variable, with percent and count labels. Good for plotting ten or
