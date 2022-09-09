21424fcf825c1c41d7143fff3076d1313987113e
Yuhua Zong <yhzong@umich.edu>
2022-09-08 23:12:31 -0700

    Learn :)

--- R/connect_awss3.R
+++ R/connect_awss3.R
@@ -7,9 +7,9 @@
 #' @return a list of dataframes
 #' @export
 
-load_s3_files <- function(s3_path = "s3://my_bucket/directory/", file_format = "csv"){
-  bucket = stringr::str_split(s3_path, "/")[[1]][3]
-  prefix = stringr::str_replace(s3_path, paste0("s3://", bucket, "/"), "")
+load_s3_files <- function(s3_path = "s3://my_bucket/directory/", file_format = "csv") {
+  bucket <-  stringr::str_split(s3_path, "/")[[1]][3]
+  prefix <- stringr::str_replace(s3_path, paste0("s3://", bucket, "/"), "")
 
   file_names <- aws.s3::get_bucket_df(bucket = bucket, prefix = prefix) %>%
     dplyr::select(Key) %>%
@@ -18,15 +18,15 @@ load_s3_files <- function(s3_path = "s3://my_bucket/directory/", file_format = "
 
   if (file_format == "csv" || file_format == "tsv") {
     # eval() evaluates an expression and parse() converts the text into an expression
-    read_fun = eval(parse(text = paste0("readr::read_", file_format)))
+    read_fun <-  eval(parse(text = paste0("readr::read_", file_format)))
   } else if (file_format == "xlsx") {
-    read_fun = eval(parse(text = paste0("readxl::read_", file_format)))
+    read_fun <-  eval(parse(text = paste0("readxl::read_", file_format)))
   }
 
   suppressMessages(
     dfs <- file_names %>%
-      lapply(function(x) aws.s3::s3read_using(FUN = read_fun,
-                                              object = paste0("s3://", bucket, "/", x)))
+      lapply(function(x) {aws.s3::s3read_using(FUN = read_fun,
+                                               object = paste0("s3://", bucket, "/", x))})
   )
 
   df_names <- file_names %>%
